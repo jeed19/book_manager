41a760a5f8f1681845acc9c884adfde039a93b2a
伊藤彰吾 <shogo.ito@ict001.com>
2026-05-27 11:44:11 +0900

    古いデータ削除とdatetimeのスペルミスの変更

--- database/migrations/2026_05_26_151342_create_employees_table.php
+++ database/migrations/2026_05_26_151342_create_employees_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->integer('EMPLOYEE_ID',8)->primarykey(); //役員識別ID用
             $table->string('password',32); 
             $table->integer('department_id',3); //各役員名用
-            $table->datetimes('joining_date'); //入社日用
+            $table->datetime('joining_date'); //入社日用
         });
     }
 
--- database/migrations/2026_05_26_151654_create_reviews_table.php
+++ database/migrations/2026_05_26_151654_create_reviews_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->integer('EMPLOYEE_ID',8); //部署ID
-            $table->integer('ISBN',13); //書籍識別番号
+            $table->integer('ISBN',13)(); //書籍識別番号
             $table->string('comment',1000); //書籍感想コメント用
             $table->integer('recommended_level',1); //書籍おすすめ度用
             $table->timestamps();
-            $table->primary(['EMPLOYEE_ID','ISBN']);
+            $table->primarykey(['EMPLOYEE_ID','ISBN']);
         });
     }
 
