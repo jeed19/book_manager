3ef95424d101885e31c8414ad58d4d072f6b0e70
Shogo <shogo.ito@gmail.com>
2026-05-27 17:46:27 +0900

    ４人の社員登録、1人は経理部で他3人は社員

--- database/seeders/EmployeesSeeder.php
+++ database/seeders/EmployeesSeeder.php
@@ -12,11 +12,30 @@ class EmployeesSeeder extends Seeder
      */
     public function run(): void
     {
+        // ４人の社員登録、１人は経理部、他３人は社員
         DB::table('employees')->insert([
-            'EMPLOYEE_ID'=> 10000000,
-            'password' => 'Ict202601',
+            'EMPLOYEE_ID' => fake()->unique()->numberBetween(10000000,99999999),
+            'password' => Hash::make('Ict202601'),
             'department_id' => 200,
-            
+            'joining_date' => '2026-05-26'
+        ]);
+        DB::table('employees')->insert([
+            'EMPLOYEE_ID' => fake()->unique()->numberBetween(10000000,99999999),
+            'password' => Hash::make('Ict202601'),
+            'department_id' => 400,
+            'joining_date' => '2026-05-26'
+        ]);
+        DB::table('employees')->insert([
+            'EMPLOYEE_ID' => fake()->unique()->numberBetween(10000000,99999999),
+            'password' => Hash::make('Ict202601'),
+            'department_id' => 400,
+            'joining_date' => '2026-05-26'
+        ]);
+        DB::table('employees')->insert([
+            'EMPLOYEE_ID' => fake()->unique()->numberBetween(10000000,99999999),
+            'password' => Hash::make('Ict202601'),
+            'department_id' => 400,
+            'joining_date' => '2026-05-26'
         ]);
     }
 }
