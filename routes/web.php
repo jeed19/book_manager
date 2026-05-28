be529ceea2a57231f47254b140c002721698a712
Shogo <shogo.ito@gmail.com>
2026-05-28 19:18:54 +0900

    Web.phpへReviewsControllerファイルの追加項目記述

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\ReviewsController;
 
 Route::get('/', function () {
     return view('index');
@@ -11,3 +12,7 @@ Route::get('/', function () {
 Route::get('books/index',[BooksController::class,'index']);
 
 Route::post('/book_manager/login',[UsersController::class,'login']);
+Route::get('/books/{isbn}/reviews/edit',[ReviewsController::class,'edit'])->name('reviews.edit');
+Route::post('/books/{isbn}/reviews',[ReviewsController::class,'store'])->name('reviews.store');
+Route::get('reviews/{id}/erase',[ReviewsController::class,'delete'])->name('reviews.erase');
+Route::post('reviews/{id}/delete',[ReviewsController::class,'delete'])->name('reviews.delete');
