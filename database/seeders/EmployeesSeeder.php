8eb696d3bf048edd5f42e591ce946304f1494c39
日根野 <hineno@mail.com>
2026-05-28 11:16:09 +0900

    シーダー修正（従業員IDはシンプルな番号に）

--- database/seeders/EmployeesSeeder.php
+++ database/seeders/EmployeesSeeder.php
@@ -15,28 +15,28 @@ class EmployeesSeeder extends Seeder
     {
         // ４人の社員登録、１人は経理部、他３人は社員
         DB::table('employees')->insert([
-            'employee_id' => fake()->unique()->numberBetween(10000000,99999999),
+            'employee_id' => 10000000,
             'password' => 'Ict202601',
             'department_id' => 200,
             'employee_name' => '伊藤',
             'display_name' => '伊藤'
         ]);
         DB::table('employees')->insert([
-            'employee_id' => fake()->unique()->numberBetween(10000000,99999999),
+            'employee_id' => 14000000,
             'password' => 'Ict202601',
             'department_id' => 400,
             'employee_name' => '高城',
             'display_name' => '高城'
         ]);
         DB::table('employees')->insert([
-            'employee_id' => fake()->unique()->numberBetween(10000000,99999999),
+            'employee_id' => 16000000,
             'password' => 'Ict202601',
             'department_id' => 400,
             'employee_name' => '寺岡',
             'display_name' => '寺岡'
         ]);
         DB::table('employees')->insert([
-            'employee_id' => fake()->unique()->numberBetween(10000000,99999999),
+            'employee_id' => 19000000,
             'password' => 'Ict202601',
             'department_id' => 400,
             'employee_name' => '日根野',
