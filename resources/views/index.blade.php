c0fe86a4317b041bba5f1feba62d6466c9be555f
日根野 <hineno@mail.com>
2026-05-28 13:06:46 +0900

    ログイン機能実装（に係り、マイグレーション、シーダー、ルータ修正。パスワードはハッシュ化されてDBに保存される）

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ラクーン社 書籍管理システム</h1>
+    <form action="/book_manager/login" method="post">
+        @csrf
+        社員ID <input type="text" name="employee_id" id="employee_id" minlength="8" maxlength ="8" value="{{ old('employee_id')}}" required><br>
+        パスワード <input type="password" name="password" id="password" minlength="8" maxlength ="32" value="{{ old('password')}}" required /><br>
+        <input type="submit" value="ログイン">
+    </form>
+    <br>
+    @if($errors->any())
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    @endif
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\UsersController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::post('/book_manager/login',[UsersController::class,'login']);
