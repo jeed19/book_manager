244492e3b08ba9b81aed6107782b957b677cb460
日根野 <hineno@mail.com>
2026-05-29 15:13:52 +0900

    books/index リンク修正

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -38,7 +38,7 @@
         @if(session('session_data')['can_register_book'])
         <li><a href="/books/create">書籍作成</a></li>
         @endif
-        <li><a href="/Employees/edit">ユーザ表示名・パスワード変更</a></li>
+        <li><a href="/employees/edit">ユーザ表示名・パスワード変更</a></li>
     
     @if(session('session_data')['can_unlock'])
     <li><a href="/employees/index" style="color: blue; font-weight: bold;">【管理者用】アカウントロック解除画面</a></li>
