02bf1f3af57cea6215425fa7c3e95a595a7cf594
伊藤彰吾 <shogo.ito@ict001.com>
2026-05-29 10:57:52 +0900

    タイトル名追加

--- database/migrations/2026_05_26_151654_create_reviews_table.php
+++ database/migrations/2026_05_26_151654_create_reviews_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('reviews', function (Blueprint $table) {
             $table->integer('EMPLOYEE_ID'); //部署ID
             $table->bigInteger('ISBN'); //書籍識別番号
+            $table->string('title',50)->nullable();; //書籍タイトル用
             $table->string('comment',1000)->nullable();; //書籍感想コメント用
             $table->integer('recommended_level')->nullable();; //書籍おすすめ度用
             $table->timestamps();
