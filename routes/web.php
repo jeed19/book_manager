677269a98fc30f0b3426a4646d056badfe78ff0d
伊藤彰吾 <shogo.ito@ict001.com>
2026-06-02 09:52:59 +0900

    レビュー削除のルート修正

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::post('/books/{isbn}/reviews/delete', [App\Http\Controllers\ReviewsControl
 Route::get('book_manager/books/{isbn}/show', [App\Http\Controllers\ReviewsController::class, 'show'])->name('reviews.show');
 Route::get('book_manager/books/{isbn}/show', [BooksController::class, 'show'])->name('books.show');
 Route::get('book_manager/books/{isbn}/reviews', [App\Http\Controllers\ReviewsController::class, 'showReviewsDetail'])->name('reviews.show');
+Route::delete('books/{isbn}/reviews/delete', [App\Http\Controllers\ReviewsController::class, 'delete'])->name('reviews.delete');
 
 // ログアウト
 Route::post('/logout', [EmployeesController::class, 'logout'])->name('logout');
